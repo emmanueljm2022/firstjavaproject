facc1844f57c75c4f153072f0b971f0cbede5efa
emmanueljm <emmanueljose1994@gmail.com>
2022-02-24 21:22:28 -0500

    First check-in

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        
+        int marks[][] = new int[][]{{1,2,3,4,5},{5,4,3,2,1},{9,8,7,6,5}};
+        for (int i=0;i <3;i++){
+            for (int j=0;j <5;j++){
+                System.out.print(marks[i][j]);
+            }
+
+            System.out.println();}
+
+        }
+
+}
