e90876594bce8c87d2808531deb2b0fd08b8b885
emmanueljm <emmanueljose1994@gmail.com>
2022-03-03 20:39:01 -0500

    added a emplyee file

---
+++ src/com/company/Employee.java
@@ -0,0 +1,37 @@
+package com.company;
+
+public class Employee {
+private int id;
+private String name;
+private String address;
+public  Employee(){
+    System.out.println("default constr is called");
+}
+public Employee(int id, String name, String address){
+    this.id= id;
+    this.name= name;
+    this.address= address;
+
+}
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+}
